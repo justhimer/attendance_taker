4bc3b9ba85044e4a4953eb14daae3b9fad5d5aff
justhimer <>
2024-05-11 22:52:22 +0800

    redo frontend.

---
+++ frontend/app/(tabs)/thirdPage.tsx
@@ -0,0 +1,16 @@
+// rnfes
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const thirdPage = () => {
+  return (
+    <View>
+      <Text>thirdPage</Text>
+    </View>
+  )
+}
+
+export default thirdPage
+
+const styles = StyleSheet.create({})
