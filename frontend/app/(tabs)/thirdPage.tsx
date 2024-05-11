95eabaf7f9d9969fecb1df7c64ce912ccdbb162c
justhimer <>
2024-05-12 00:28:35 +0800

    drafted layout of frontend.

--- frontend/app/(tabs)/thirdPage.tsx
+++ frontend/app/(tabs)/thirdPage.tsx
@@ -1,16 +1,15 @@
-// rnfes
+// // rnfe
 
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 const thirdPage = () => {
   return (
-    <View>
-      <Text>thirdPage</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">thirdPage</Text>
     </View>
   )
 }
 
 export default thirdPage
-
-const styles = StyleSheet.create({})
